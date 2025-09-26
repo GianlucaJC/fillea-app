be937d15b48228d87c8c308ce6d5b670d2aad81d
GianlucaJC <morescogianluca@gmail.com>
2025-09-26 16:53:59 +0200

    Update ricerca

--- resources/views/lavoratore/main.blade.php
+++ resources/views/lavoratore/main.blade.php
@@ -16,7 +16,7 @@
         .container {
             max-width: 600px;
             background-color: #fff;
-            padding: 2rem;
+            padding: 1rem;
             border-radius: 0.5rem;
             box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
         }
@@ -29,10 +29,10 @@
         <input name="_token" type="hidden" value="{{ csrf_token() }}" id='token_csrf'>
         <input type="hidden" value="{{url('/')}}" id="url" name="url">
         
-        <h1 class="text-center mb-4">INSERISCI I TUOI DATI</h1>
+        <h3 class="text-center mb-3">INSERISCI I TUOI DATI</h3>
 
         <!-- Settore -->
-        <div class="mb-3">
+        <div class="mb-1">
             <label for="settore" class="form-label text-warning">SETTORE</label>
             <select id="settore" name="settore" class="form-select" required >
                 <option value="">Seleziona un settore</option>
@@ -50,12 +50,12 @@
 
         <!-- Nome e Cognome -->
         <div class="row">
-            <div class="col-md-6 mb-3">
+            <div class="col-md-6 mb-2">
                 <label for="cognome" class="form-label text-warning">COGNOME</label>
                 <input type="text" id="cognome" name="cognome" class="form-control" required>
                 <div class="invalid-feedback">Per favore, specifica il cognome.</div>
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-6 mb-2">
                 <label for="nome" class="form-label text-warning">NOME</label>
                 <input type="text" id="nome" name="nome" class="form-control" required >
                 <div class="invalid-feedback">Per favore, specifica il nome.</div>
@@ -64,12 +64,12 @@
 
         <!-- Data di Nascita e Codice Fiscale -->
         <div class="row">
-            <div class="col-md-6 mb-3">
+            <div class="col-md-6 mb-2">
                 <label for="data_nascita" class="form-label text-warning">DATA DI NASCITA*</label>
                 <input type="date" id="data_nascita" name="data_nascita" class="form-control" required>
                 <div class="invalid-feedback">Per favore, specifica la data di nascita.</div>
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-6 mb-2">
                 <label for="codfisc" class="form-label text-warning">CODICE FISCALE</label>
                 <input type="text" id="codfisc" name="codfisc" class="form-control" required>
                 <div class="invalid-feedback">Per favore, specifica il codice fiscale.</div>
@@ -78,23 +78,23 @@
 
         <!-- Telefono e Email -->
         <div class="row">
-            <div class="col-md-6 mb-3">
+            <div class="col-md-6 mb-2">
                 <label for="telefono" class="form-label text-warning">TELEFONO CELLULARE*</label>
                 <input type="tel" id="telefono" name="telefono" class="form-control" required>
                 <div class="invalid-feedback">Per favore, specifica il telefono.</div>
             </div>
-            <div class="col-md-6 mb-3">
+            <div class="col-md-6 mb-2">
                 <label for="email" class="form-label">EMAIL</label>
                 <input type="email" id="email" name="email" class="form-control">
             </div>
         </div>
         <div id='div_wait' style='display:none'><i class='fas fa-spinner fa-spin'></i></div>
         <div id='div_resp'></div>
-        <div class="d-grid mt-4">
+        <div class="d-grid mt-3">
             <button type="submit" class="btn btn-primary btn-lg">CLICCA PER INIZIARE</button>
         </div>
 
-        <div class="text-center mt-3">
+        <div class="text-center mt-1">
             <p class="text-warning">IN GIALLO I CAMPI OBBLIGATORI</p>
         </div>
     </form>
